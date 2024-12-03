7e59b084a3cf3ae019620b0b463303e82d567640
Nguyễn Quân <92988230+icecnguyen@users.noreply.github.com>
2024-12-03 21:04:59 +0700

    Delete recaptcha.js

--- recaptcha.js
+++
@@ -1,9 +0,0 @@
-function loadRecaptcha() {
-  var script = document.createElement('script');
-  script.src = 'https://www.google.com/recaptcha/api.js';
-  document.head.appendChild(script);
-}
-
-function onRecaptchaSuccess(token) {
-  console.log('Recaptcha successful with token: ' + token);
-}
