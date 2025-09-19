9499d9a6cdcc77965d37fec0244c5806539a4a0d
icecnguyen <92988230+icecnguyen@users.noreply.github.com>
2025-09-19 18:10:59 +0700

    Update levelloader.js

--- assets/js/levelloader.js
+++ assets/js/levelloader.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const container = document.getElementById("level-container");
 
   try {
-    const res = await fetch("/../assets/data/maps.json");
+    const res = await fetch("assets/data/maps.json");
     const data = await res.json();
 
     const urlParams = new URLSearchParams(window.location.search);
@@ -98,6 +98,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.error(err);
   }
 });
+
 
 
 
